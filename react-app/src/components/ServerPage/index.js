d7d2d2126ae2560a9227e2e2e0a8be965ba4c608
hector <crespomhector@gmail.com>
2024-06-19 11:43:03 -0400

    fix bug with conditionally rendering the update/delete server button only if the user owns the server on the server component page.

--- react-app/src/components/ServerPage/index.js
+++ react-app/src/components/ServerPage/index.js
@@ -24,13 +24,13 @@ const ServerPage = () => {
   const channels = useSelector((state) => {
     return Object.values(state.channel);
   });
-  const servers = useSelector((state) => {
-    return Object.values(state?.servers?.allServers);
-  });
   const categories = useSelector((state) => {
     return Object.values(state.category);
   })
   const usersByServer = useSelector((state) => state.user_server.user)
+  const user = useSelector((state) => state.session.user)
+
+  const isOwner = server?.owner_id === user?.id;
 
   // const messagesEndRef = useRef(null)
 
@@ -62,131 +62,129 @@ const ServerPage = () => {
     setOpen(false)
   }
 
-  if (!servers) {
-    return null
-  } else {
-    const handleOpen = (channel) => {
-      setChannel(channel)
-      setOpen(true);
-    };
+  const handleOpen = (channel) => {
+    setChannel(channel)
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-      setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    // handle leave server button
-    const leaveServer = (e) => {
-      // dispatch leave server thunk action
-      e.preventDefault();
-      dispatch(removeMemberFromServer(server?.id))
-        .then(res => {
-          history.push(`/`)
-        })
-        .catch(err => {
-          console.log('error: ', err);
-        })
-      // after submission, redirect user to the discover page
-    }
+  // handle leave server button
+  const leaveServer = (e) => {
+    // dispatch leave server thunk action
+    e.preventDefault();
+    dispatch(removeMemberFromServer(server?.id))
+      .then(res => {
+        // after submission, redirect user to the discover page
+        history.push(`/`)
+      })
+      .catch(err => {
+        console.log('error: ', err);
+      })
+  }
 
-
-
-    return (
-      <div className="server-page">
-        <Modal
-          open={open}
-          onClose={handleClose}
-        >
-          <div id="modal_channel">
-            <h1>Edit/Delete Channel</h1>
-            <form className="form">
-              <label htmlFor="channel-name" className="edit-label">
-                Edit Channel
-              </label>
-              <input
-                type="text"
-                name="channel_name"
-                className="form-input"
-                value={channelTitle}
-                onChange={(e) => setChannelTitle(e.target.value)}
-                required
-              ></input>
-              <button
-                type="submit"
-                id="edit-form_button"
-                onClick={onClickEditChannel}
-              >
-                Edit Channel
-              </button>
-              <button type="button" onClick={onClickDeleteChannel} className="delete-btn_channel">Delete Channel</button>
-            </form>
-          </div>
-        </Modal>
-
-        <div className="name">
-          <div>{server?.name}</div>
-          <button className="server-btn">
-            <NavLink to={`/servers/${id}/update`} id="textt">
-              update
-            </NavLink>
-          </button>
-          <button className="server-btn">
-            <NavLink to={`/servers/${id}/delete`} id="textt">
-              delete
-            </NavLink>
-          </button>
+  return (
+    <div className="server-page">
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <div id="modal_channel">
+          <h1>Edit/Delete Channel</h1>
+          <form className="form">
+            <label htmlFor="channel-name" className="edit-label">
+              Edit Channel
+            </label>
+            <input
+              type="text"
+              name="channel_name"
+              className="form-input"
+              value={channelTitle}
+              onChange={(e) => setChannelTitle(e.target.value)}
+              required
+            ></input>
+            <button
+              type="submit"
+              id="edit-form_button"
+              onClick={onClickEditChannel}
+            >
+              Edit Channel
+            </button>
+            <button type="button" onClick={onClickDeleteChannel} className="delete-btn_channel">Delete Channel</button>
+          </form>
         </div>
+      </Modal>
 
-        <UserBar />
+      <div className="name">
+        <div>{server?.name}</div>
+        { isOwner && (
+          <>
+            <button className="server-btn">
+              <NavLink to={`/servers/${id}/update`} id="textt">
+                update
+              </NavLink>
+            </button>
+            <button className="server-btn">
+              <NavLink to={`/servers/${id}/delete`} id="textt">
+                delete
+              </NavLink>
+            </button>
+          </>
+        )}
+      </div>
 
-        <div className="categories">
-          <div>
-            {categories.map((category) => (
-              <div key={category.id} id="category" className="channel">
-                {`${category.title.toUpperCase()}`}
-                <ul className="text-channels">
-                  {channels?.map((channel) =>
-                    channel.category_id === category.id ? (
-                      <NavLink key={channel.id} to={`/servers/${server.id}/channel/${channel.id}`}>
-                        <li className="channel">
-                          {" "}
-                          {`${channel.title}`}
-                          <button
-                            type="button"
-                            onClick={() => handleOpen(channel)}
-                            className="edit-channel"
-                          >
-                            ⚙
-                          </button>
-                        </li>
-                      </NavLink>
-                    ) : null
-                  )}
-                </ul>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="chat-div">
-          <Chat />
-        </div>
-        <div className="sqr">
-        </div>
-        <div className="channel-name">
-          <span className="channel-text"># channel</span>
-          <button className="leave-server-btn" onClick={(e) => leaveServer(e)}>Leave Server</button>
-        </div>
-        <div className="members-div">
-          {usersByServer?.map((user) => (
-            <li key={user.id} className="user">{`${user.username}`}</li>
+      <UserBar />
+
+      <div className="categories">
+        <div>
+          {categories.map((category) => (
+            <div key={category.id} id="category" className="channel">
+              {`${category.title.toUpperCase()}`}
+              <ul className="text-channels">
+                {channels?.map((channel) =>
+                  channel.category_id === category.id ? (
+                    <NavLink key={channel.id} to={`/servers/${server.id}/channel/${channel.id}`}>
+                      <li className="channel">
+                        {" "}
+                        {`${channel.title}`}
+                        <button
+                          type="button"
+                          onClick={() => handleOpen(channel)}
+                          className="edit-channel"
+                        >
+                          ⚙
+                        </button>
+                      </li>
+                    </NavLink>
+                  ) : null
+                )}
+              </ul>
+            </div>
           ))}
         </div>
-        <div className="options"></div>
-        <button className="about-btn">
-          <About />
-        </button>
       </div>
-    );
-  }
+      <div className="chat-div">
+        <Chat />
+      </div>
+      <div className="sqr">
+      </div>
+      <div className="channel-name">
+        <span className="channel-text"># channel</span>
+        <button className="leave-server-btn" onClick={(e) => leaveServer(e)}>Leave Server</button>
+      </div>
+      <div className="members-div">
+        {usersByServer?.map((user) => (
+          <li key={user.id} className="user">{`${user.username}`}</li>
+        ))}
+      </div>
+      <div className="options"></div>
+      <button className="about-btn">
+        <About />
+      </button>
+    </div>
+  );
 }
 
 export default ServerPage;
