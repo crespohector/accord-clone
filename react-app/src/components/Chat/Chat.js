072ecd8f6f9aed21181bad943361fe68c68186e1
hector <crespomhector@gmail.com>
2024-06-26 22:01:15 -0400

    refactored useEffect in the chat component to unmount the websocket if channelID's changes. If disconnected, will attempt to reconnect websockets. Fixed bug where users can see incoming messages from different channels.

--- react-app/src/components/Chat/Chat.js
+++ react-app/src/components/Chat/Chat.js
@@ -6,11 +6,10 @@ import { chatPost, chatForChannel } from "../../store/chats"
 import { deleteChat } from "../../store/chats";
 import './index.css';
 
-const socket = io('http://127.0.0.1:5000/');
+let socket = io('http://127.0.0.1:5000/');
 
 const Chat = ({ server, channels }) => {
     const [chatInput, setChatInput] = useState("");
-    const [messages, setMessages] = useState([]);
     const [channel, setChannel] = useState()
     const user = useSelector(state => state.session.user)
     const dispatch = useDispatch();
@@ -26,25 +25,41 @@ const Chat = ({ server, channels }) => {
         divRef.current.scrollIntoView({ behavior: 'smooth' });
     });
 
+    // useEffect(() => {
+    //     // render the chat messages on the selected channel
+    //     if (channelId) {
+    //         dispatch(chatForChannel(channelId))
+    //     }
+    // }, [dispatch, channelId])
+
     useEffect(() => {
+        console.log('HIT----')
         // render the chat messages on the selected channel
         if (channelId) {
             dispatch(chatForChannel(channelId))
+
+            console.log('socket: ', socket)
+            if (socket.disconnected) {
+                console.log('reconnect---')
+                socket = io('http://127.0.0.1:5000/');
+            }
+
+            // listen for chat messages from the server
+            socket.on("chat", (data) => {
+                // unmount if channel ids do not match
+                // render incoming chat message only in the correct channel
+                console.log('CHAT-----: ', data, channelId);
+                console.log(data?.chat.channel_id == channelId)
+                if (data?.chat.channel_id == channelId) {
+                    dispatch(chatPost(data))
+                }
+            })
         }
-    }, [dispatch, channelId])
-
-    useEffect(() => {
-        // listen for chat messages from the server
-        socket.on("chat", (data) => {
-            console.log('CHAT-----: ', data);
-            // setMessages(messages => [...messages, chat])
-            dispatch(chatPost(data))
-        })
-
         return (() => {
-            socket.disconnect()
+                console.log('unmount------')
+                socket.disconnect()
         })
-    }, [])
+    }, [channelId])
 
     const sendChat = async (e) => {
         e.preventDefault()
@@ -53,29 +68,29 @@ const Chat = ({ server, channels }) => {
         setChatInput("")
     }
 
-    const handleDeleteChat = (e, {id}) => {
+    const handleDeleteChat = (e, { id }) => {
         e.preventDefault();
         dispatch(deleteChat(id));
     }
 
     const ShowChats = () => {
-            return chats?.map((chat) => {
-                const isSessionUser = chat.owner_id === user.id;
-                return (
-                    <div id="previousMessages" key={chat.id}>
-                        <div className={isSessionUser ? "chat_user-active" : "Chat_user"}>
-                            {chat.user.username}
-                            {server.owner_id === chat.user.id && (
-                                <small id="admin-small-text">server admin</small>
-                            )}
-                            {isSessionUser && (
-                                <button id="btn-delete-chat" type="button" onClick={(e) => handleDeleteChat(e, chat)}>delete</button>
-                            )}
-                        </div>
-                        <div id="Chat_message">{chat.content}</div>
+        return chats?.map((chat) => {
+            const isSessionUser = chat.owner_id === user.id;
+            return (
+                <div id="previousMessages" key={chat.id}>
+                    <div className={isSessionUser ? "chat_user-active" : "Chat_user"}>
+                        {chat.user.username}
+                        {server.owner_id === chat.user.id && (
+                            <small id="admin-small-text">server admin</small>
+                        )}
+                        {isSessionUser && (
+                            <button id="btn-delete-chat" type="button" onClick={(e) => handleDeleteChat(e, chat)}>delete</button>
+                        )}
                     </div>
-                );
-            })
+                    <div id="Chat_message">{chat.content}</div>
+                </div>
+            );
+        })
     }
 
     return (user && (
